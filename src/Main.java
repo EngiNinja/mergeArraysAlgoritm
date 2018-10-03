aa8aa8b3af6bfe5415b3ebb0965f7003389ebbd2
azamaraj <zamarajev@gmail.com>
2018-10-03 14:06:40 +0200

    better solution found

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,36 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
-    /*
-    * @param array1 array elements from which are kept
-    * @param array2 array elements from which are deleted
-     *finds elements from first array that are not in second
-     */
-    public static ArrayList<Integer> findInFirstButNotInSecond(int[] array1, int[] array2) {
-        ArrayList<Integer> output = new ArrayList<>();
-
-        int iterArray1 = 0;
-        int iterArray2 = 0;
-        int p1 = array1[iterArray1];
-        int p2 = array2[iterArray2];
-        Integer lastP2 = p2;
-        Integer lastInsertedP1 = 0;
-        boolean lastIter = false;
-
-        while (true) {
-            // cheks if its the last iteration or if it should stop
-            if ((iterArray1 == array1.length - 1) && (iterArray2 == array2.length - 1)
-                && lastIter) {
-                break;
-            } else if ((iterArray1 == array1.length - 1) && (iterArray2 == array2.length - 1)) {
-                lastIter = true;
+    public static int[] findInFirstButNotInSecond(int[] src, int[] excluded) {
+        int[] dest = new int[src.length];
+        int iDest = 0;
+        int iSrc = 0;
+        int iExcl = 0;
+        while (iSrc < src.length) {
+            if (iExcl >= excluded.length || src[iSrc] < excluded[iExcl]) {
+                // If source element is smaller than the next excluded element, or there are no more excluded elements
+                // add it to the destination and move onto the next one
+                dest[iDest] = src[iSrc];
+                iSrc++;
+                iDest++;
+            } else if (src[iSrc] > excluded[iExcl]) {
+                // If source element is larger, move the pointer on excluded array forward and re-evaluate
+                iExcl++;
+            } else if (src[iSrc] == excluded[iExcl]) {
+                // If both elements are equal, we have a hit - ignore and move on
+                iSrc++;
+                iExcl++;
             }
-
-            // magic
-            if (p1 > p2) {
-                if (iterArray2 != array2.length - 1) {
-                    iterArray2++;
-                } else {
-                    if ((output.isEmpty()) || ((output.get(output.size() - 1) != p1) && (p1
-                        != lastP2) && p1 != lastInsertedP1)) {
-                        output.add(p1);
-                        lastInsertedP1 = p1;
-                    }
-                    if (iterArray1 != array1.length - 1) {
-                        iterArray1++;
-                    }
-                }
-            } else if (p1 < p2) {
-                if ((output.isEmpty()) || ((output.get(output.size() - 1) != p1) && (p1 != lastP2)
-                    && p1 != lastInsertedP1)) {
-                    output.add(p1);
-                    lastInsertedP1 = p1;
-                }
-                if (iterArray1 != array1.length - 1) {
-                    iterArray1++;
-                } else if (iterArray2 != array2.length - 1) {
-                    iterArray2++;
-                }
-            } else if (p1 == p2) {
-                if (iterArray2 != array2.length - 1) {
-                    iterArray2++;
-                }
-                if (iterArray1 != array1.length - 1) {
-                    iterArray1++;
-                }
-            }
-            lastP2 = p2;
-            p1 = array1[iterArray1];
-            p2 = array2[iterArray2];
         }
-        return output;
+
+        // Return only the filled part of the array
+        int[] result = new int[iDest];
+        System.arraycopy(dest, 0, result, 0, iDest);
+        return result;
     }
 
     public static void main(String[] args) {
@@ -76,51 +41,51 @@ public class Main {
 
         System.out.println(Arrays.toString(array1));
         System.out.println(Arrays.toString(array2));
-        System.out.println(findInFirstButNotInSecond(array1, array2));
+        System.out.println(Arrays.toString(findInFirstButNotInSecond(array1, array2)));
         System.out.println();
         System.out.println(Arrays.toString(array1));
         System.out.println(Arrays.toString(array3));
-        System.out.println(findInFirstButNotInSecond(array1, array3));
+        System.out.println(Arrays.toString(findInFirstButNotInSecond(array1, array3)));
         System.out.println();
         System.out.println(Arrays.toString(array1));
         System.out.println(Arrays.toString(array4));
-        System.out.println(findInFirstButNotInSecond(array1, array4));
+        System.out.println(Arrays.toString(findInFirstButNotInSecond(array1, array4)));
         System.out.println();
         System.out.println(Arrays.toString(array2));
         System.out.println(Arrays.toString(array3));
-        System.out.println(findInFirstButNotInSecond(array2, array3));
+        System.out.println(Arrays.toString(findInFirstButNotInSecond(array2, array3)));
         System.out.println();
         System.out.println(Arrays.toString(array2));
         System.out.println(Arrays.toString(array4));
-        System.out.println(findInFirstButNotInSecond(array2, array4));
+        System.out.println(Arrays.toString(findInFirstButNotInSecond(array2, array4)));
         System.out.println();
         System.out.println(Arrays.toString(array3));
         System.out.println(Arrays.toString(array4));
-        System.out.println(findInFirstButNotInSecond(array3, array4));
+        System.out.println(Arrays.toString(findInFirstButNotInSecond(array3, array4)));
         System.out.println();
         System.out.println(Arrays.toString(array2));
         System.out.println(Arrays.toString(array1));
-        System.out.println(findInFirstButNotInSecond(array2, array1));
+        System.out.println(Arrays.toString(findInFirstButNotInSecond(array2, array1)));
         System.out.println();
         System.out.println(Arrays.toString(array3));
         System.out.println(Arrays.toString(array1));
-        System.out.println(findInFirstButNotInSecond(array3, array1));
+        System.out.println(Arrays.toString(findInFirstButNotInSecond(array3, array1)));
         System.out.println();
         System.out.println(Arrays.toString(array4));
         System.out.println(Arrays.toString(array1));
-        System.out.println(findInFirstButNotInSecond(array4, array1));
+        System.out.println(Arrays.toString(findInFirstButNotInSecond(array4, array1)));
         System.out.println();
         System.out.println(Arrays.toString(array3));
         System.out.println(Arrays.toString(array2));
-        System.out.println(findInFirstButNotInSecond(array3, array2));
+        System.out.println(Arrays.toString(findInFirstButNotInSecond(array3, array2)));
         System.out.println();
         System.out.println(Arrays.toString(array4));
         System.out.println(Arrays.toString(array2));
-        System.out.println(findInFirstButNotInSecond(array4, array2));
+        System.out.println(Arrays.toString(findInFirstButNotInSecond(array4, array2)));
         System.out.println();
         System.out.println(Arrays.toString(array4));
         System.out.println(Arrays.toString(array3));
-        System.out.println(findInFirstButNotInSecond(array4, array3));
+        System.out.println(Arrays.toString(findInFirstButNotInSecond(array4, array3)));
         System.out.println();
     }
 }
